cfbcc1c922e3152aef6319ecb4892249afb8bbe3
David-Martinez-Mercado <mtzdavid2004@hotmail.com>
2026-09-16 19:11:52 -0600

    Mueve la URL de la API a environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+
+  /** En produccion la API deberia ir por HTTPS y con un dominio real. */
+  apiUrl: 'http://localhost/api/usuarios.php',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  /**
+   * URL base de la API en PHP.
+   *
+   * localhost solo funciona desde el navegador de la computadora. Desde el
+   * emulador de Android hay que usar 10.0.2.2, y desde un celular fisico la
+   * IP de la computadora en la red local:
+   *
+   *   Emulador Android:    'http://10.0.2.2/api/usuarios.php'
+   *   Dispositivo fisico:  'http://192.168.1.70/api/usuarios.php'
+   */
+  apiUrl: 'http://localhost/api/usuarios.php',
 };
